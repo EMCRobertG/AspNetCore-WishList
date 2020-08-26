593f41bfe8b0e1436ce8c10f9573118d105926a6
EMCRobertG <70227824+EMCRobertG@users.noreply.github.com>
2020-08-26 08:06:50 -0500

    Added .Controllers

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -6,20 +6,20 @@ using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace WishList
+namespace WishList.Controllers
 {
     public class HomeController : Controller
     {
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
 
         // Error
         public IActionResult Error()
         {
-            return View();
+            return View("Error");
         }
     }
 }
