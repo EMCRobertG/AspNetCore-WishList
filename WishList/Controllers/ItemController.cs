09be64f306a4f6fc156b029e81e53679866f3a99
EMCRobertG <70227824+EMCRobertG@users.noreply.github.com>
2020-08-26 13:32:51 -0500

    Module 6

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        private ApplicationDbContext _context;
+
+        public void SetContext(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+           // SetContext(item);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            // SetContext(item);
+            return RedirectToAction("Index");
+        }
+    }
+}
